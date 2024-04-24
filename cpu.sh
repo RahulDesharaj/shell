901651e93877b79f67d8e366c841084bee965ff2
RahulDesharaj <rahuldesharaj92@gmail.com>
2024-04-24 22:01:43 +0530

    shell

--- cpu.sh
+++ cpu.sh
@@ -11,10 +11,7 @@ CPU_USAGE=$(ps -eo pid,comm,%cpu,%mem --sort=-%cpu | head -n 5)
 
 MAX_CPU_USAGE=05
 # Check if the CPU usage is above the threshold
-
-
-
-if [ "$CPU_USAGE" -lt "$MAX_CPU_USAGE" ]  
+if [[ "$CPU_USAGE" -le 7 || "$MAX_CPU_USAGE" -ge 65 ]]  
 then
     # Construct the message to send to Discord
     
