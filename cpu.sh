93ef4af46fb41c43d742cb4818c0cf1d54ceafc9
RahulDesharaj <rahuldesharaj92@gmail.com>
2024-04-24 22:34:16 +0530

    shell

--- cpu.sh
+++ cpu.sh
@@ -7,22 +7,27 @@ G="\e[32m"
 
 N="\e[0m"
 
-CPU_USAGE=$(ps --sort=-pcpu | head -n 6)
+#CPU_USAGE=$(ps --sort=-pcpu | head -n 6)
 
-MAX_CPU_USAGE=05
-# Check if the CPU usage is above the threshold
-#if [ $CPU_USAGE -ge $MAX_CPU_USAGE ]
-#if [ "$CPU_USAGE" -ge "$MAX_CPU_USAGE" ]
-if [ $CPU_USAGE -gt 10 ]
 
+# if [ $CPU_USAGE -gt 10 ]
+
+# then
+#     # Construct the message to send to Discord
+    
+#     echo  -e "$R CPU usage is HIGH $N"
+#     exit 1
+# else
+    
+#     echo  -e "$G CPU usage is NORMAL $N"
+
+# fi
+
+NUMBER=$(ps --sort=-pcpu | head -n 6)
+
+if [ $NUMBER -gt 10 ]
 then
-    # Construct the message to send to Discord
-    
     echo  -e "$R CPU usage is HIGH $N"
-    exit 1
 else
-    
-    echo  -e "$G CPU usage is NORMAL $N"
-
+    echo  -e "$G CPU usage is NORMAL $N" 
 fi
-
