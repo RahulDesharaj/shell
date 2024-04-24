edb7daff935ef451ad05612bcbe0fb6e2b4e1636
RahulDesharaj <rahuldesharaj92@gmail.com>
2024-04-24 21:54:43 +0530

    shell

--- cpu.sh
+++ cpu.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 echo "display the top 5 cpu consuming process in linux server"
+
+R="\e[31m"
+
+G="\e[32m"
+
+N="\e[0m"
+
 CPU_USAGE=$(ps -eo pid,comm,%cpu,%mem --sort=-%cpu | head -n 5)
 
-MAX_CPU_USAGE=40
+MAX_CPU_USAGE=05
 # Check if the CPU usage is above the threshold
+
+
+
 if [ "$CPU_USAGE" -ge "$MAX_CPU_USAGE" ]  
 then
     # Construct the message to send to Discord
-    MESSAGE=" CPU usage is HIGH "
+    
+    echo  -e "$R CPU usage is HIGH $N"
     exit 1
 else
-    echo " CPU usage is NORMAL "
+    
+    echo  -e "$G CPU usage is NORMAL $N"
 
 fi
 
