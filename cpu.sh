65f9459235bdda64ab0dcbeb7a7f6a1346785452
RahulDesharaj <rahuldesharaj92@gmail.com>
2024-04-24 21:50:10 +0530

    shell

--- cpu.sh
+++ cpu.sh
@@ -4,9 +4,13 @@ CPU_USAGE=$(ps -eo pid,comm,%cpu,%mem --sort=-%cpu | head -n 5)
 
 MAX_CPU_USAGE=40
 # Check if the CPU usage is above the threshold
-if [ "$CPU_USAGE" -ge "$MAX_CPU_USAGE" ]  then
+if [ "$CPU_USAGE" -ge "$MAX_CPU_USAGE" ]  
+then
     # Construct the message to send to Discord
-    MESSAGE="CPU usage is HIGH"
+    MESSAGE=" CPU usage is HIGH "
+    exit 1
+else
+    echo " CPU usage is NORMAL "
 
 fi
 
