aa66584aa0fdc9f422ffc41b358630adabe105b6
RahulDesharaj <rahuldesharaj92@gmail.com>
2024-04-24 22:30:07 +0530

    shell

--- cpu.sh
+++ cpu.sh
@@ -12,7 +12,7 @@ CPU_USAGE=$(ps --sort=-pcpu | head -n 6)
 MAX_CPU_USAGE=05
 # Check if the CPU usage is above the threshold
 #if [ $CPU_USAGE -ge $MAX_CPU_USAGE ]
-if [ "$CPU_USAGE" -ge "$MAX_CPU_USAGE" ]
+#if [ "$CPU_USAGE" -ge "$MAX_CPU_USAGE" ]
 if [ $CPU_USAGE -gt 10 ]
 
 then
